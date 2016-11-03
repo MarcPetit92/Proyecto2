3eebc3af04fb3fce2fab029b65ca1f9149e27239
edhuchacaliaza <93191.joan23@fje.edu>
2016-11-03 16:33:35 +0100

    BBDD corregida

--- u163772754_p2ir.sql
+++ u163772754_p2ir.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-11-2016 a las 15:44:23
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 7.0.6
+-- Tiempo de generación: 03-11-2016 a las 16:31:14
+-- Versión del servidor: 10.1.10-MariaDB
+-- Versión de PHP: 5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,31 +23,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_detalle_reserva`
---
-
-CREATE TABLE `tbl_detalle_reserva` (
-  `det_id` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tbl_devolucion`
---
-
-CREATE TABLE `tbl_devolucion` (
-  `dev_id` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `tbl_recursos`
 --
 
 CREATE TABLE `tbl_recursos` (
-  `rec_id` int(4) NOT NULL
+  `rec_id` int(4) NOT NULL,
+  `rec_nombre` varchar(15) NOT NULL,
+  `rec_disponibilidad` tinyint(1) NOT NULL,
+  `rec_foto` varchar(100) NOT NULL,
+  `tip_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -57,7 +41,13 @@ CREATE TABLE `tbl_recursos` (
 --
 
 CREATE TABLE `tbl_reserva` (
-  `res_id` int(4) NOT NULL
+  `res_id` int(4) NOT NULL,
+  `res_fecha_ini` date NOT NULL,
+  `res_fecha_fin` date DEFAULT NULL,
+  `res_hora_ini` time NOT NULL,
+  `res_hora_fin` time DEFAULT NULL,
+  `rec_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -67,7 +57,8 @@ CREATE TABLE `tbl_reserva` (
 --
 
 CREATE TABLE `tbl_tipo_recurso` (
-  `tip_id` int(4) NOT NULL
+  `tip_id` int(4) NOT NULL,
+  `tip_nombre` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -77,24 +68,17 @@ CREATE TABLE `tbl_tipo_recurso` (
 --
 
 CREATE TABLE `tbl_usuario` (
-  `usu_id` int(4) NOT NULL
+  `usu_id` int(4) NOT NULL,
+  `usu_alias` varchar(10) NOT NULL,
+  `usu_pass` varchar(10) NOT NULL,
+  `usu_nombre` varchar(15) NOT NULL,
+  `usu_apellido` varchar(20) NOT NULL,
+  `usu_email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `tbl_detalle_reserva`
---
-ALTER TABLE `tbl_detalle_reserva`
-  ADD PRIMARY KEY (`det_id`);
-
---
--- Indices de la tabla `tbl_devolucion`
---
-ALTER TABLE `tbl_devolucion`
-  ADD PRIMARY KEY (`dev_id`);
 
 --
 -- Indices de la tabla `tbl_recursos`
@@ -124,16 +108,6 @@ ALTER TABLE `tbl_usuario`
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
---
--- AUTO_INCREMENT de la tabla `tbl_detalle_reserva`
---
-ALTER TABLE `tbl_detalle_reserva`
-  MODIFY `det_id` int(4) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `tbl_devolucion`
---
-ALTER TABLE `tbl_devolucion`
-  MODIFY `dev_id` int(4) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `tbl_recursos`
 --
