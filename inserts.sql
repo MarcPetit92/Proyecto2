3f98205d28b90d7385c4758e10569f0548fcc3f4
MarcPetit92 <1001489.joan23@fje.edu>
2016-11-04 15:15:48 +0100

    subiendo planning viernes, inserts e intranet

---
+++ inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO u163772754_p2ir.tbl_recursos (rec_nombre, rec_disponibilidad, rec_foto, tip_id)  
+	VALUES ('Aula Teoria 100', 1 , '1.jpg', 1);
+
+INSERT INTO u163772754_p2ir.tbl_usuario (usu_alias, usu_pass, usu_nombre, usu_apellido, usu_email)  
+	VALUES ('mpetit', '12345' , 'Marc', 'Petit', 'mpetit92@gmail.com');
